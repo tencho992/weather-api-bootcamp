78e73daa1e8be4f47250882c7a946e772f2c2f13
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-04-04 10:39:15 -0400

    thanksDannyKins

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-document.querySelector('button').addEventListener('click', getTemp)
-    
 function getTemp(){
-  let city= document.querySelector('#city').value
-  let country= document.querySelector('#country').value
-
-  const url= `http://api.weatherstack.com/current?access_key=6d7283ca10feb5870a89057ec8b615f4&query=${city}`
+  let getAnswer = document.querySelector('input').value
+  
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${getAnswer}&APPID=76cdbae952e035864405714b8200419f`
   fetch(url)
   .then(res => res.json()) // parse response as JSON
   .then(data => {
     console.log(data)
-    // document.querySelector('h2').innerText= data.title
-    
+    let convert = Math.floor((data.main.temp - 273.15) * 1.8 + 32)
+    document.querySelector('#placeToSee').innerText = `${convert}`
   })
   .catch(err => {
       console.log(`error ${err}`)
 });
 }
+document.querySelector('button').addEventListener('click', getTemp)
